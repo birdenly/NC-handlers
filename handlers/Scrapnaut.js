a9faea613b81ac18de1dbf7acd32cb3a9c3da2d0
birdenly <jomacaldas@hotmail.com.br>
2025-01-26 18:26:38 -0300

    scrapnaut

---
+++ handlers/Scrapnaut.js
@@ -0,0 +1,239 @@
+Game.ExecutableContext = ["Scrapnaut_Data"];
+Game.FileSymlinkExclusions = [
+  "steam_api64.dll",
+  "steam_appid.txt",
+  "Galaxy64.dll",
+    "GalaxyCSharpGlue.dll",
+];
+Game.FileSymlinkCopyInstead = [
+"UnityPlayer.dll",
+"globalgamemanagers"
+];
+Game.HandlerInterval = 100;
+Game.SymlinkExe = false;
+Game.SymlinkGame = true;
+Game.SymlinkFolders = false;
+Game.KeepSymLinkOnExit = false;
+Game.ExecutableName = "Scrapnaut.exe";
+Game.SteamID = "1323900";
+Game.GUID = "Scrapnaut";
+Game.GameName = "Scrapnaut";
+Game.MaxPlayers = 4;
+Game.MaxPlayersOneMonitor = 4;
+Game.HideTaskbar = true;
+Game.Hook.ForceFocus = false;
+Game.Hook.ForceFocusWindowName = "Scrapnaut";
+Game.ResetWindows = true;
+Game.Hook.DInputEnabled = false;
+Game.Hook.XInputEnabled = false;
+Game.Hook.XInputReroute = false;
+Game.Hook.CustomDllEnabled = false;
+Game.XInputPlusDll = [];
+Game.Description =
+  "IMPORTANT: YOU NEED INTERNET CONNECTION TO PLAY\n\nGo into multiplayer and host a room visible for friends, than join in with others through the multiplayer friends tab\n\nRecommended that you add custom resolutions to all your monitors from your AMD/Nvidia/Intel panel (for example if you are using a monitor resolution of 1920x1080 add custom resolutions like 1920x540, 960x1080, 960x540, etc.)\n\nIf you use keyboards and mice after all the instances have launched, resized and positioned correctly, press the END key once to lock the input for all instances to have their own working cursor and keyboard. You need to left click each mouse to make the emulated cursors appear after locking the input. Press the END key again to unlock the input when you finish playing. You can also use CTRL+Q to close Nucleus and all its instances when the input is unlocked.";
+Game.PauseBetweenProcessGrab = 5;
+Game.PauseBetweenStarts = 10;
+
+Game.UseGoldberg = true;
+Game.UseNemirtingasEpicEmu = true; //just to make sure
+Game.UseNemirtingasGalaxyEmu = true; //just to make sure
+Game.SetWindowHookStart = true;
+// Game.UseNucleusEnvironment = true;
+Game.UserProfileSavePath = "AppData\\LocalLow\\SpiffyBit\\Scrapnaut";
+
+//USS deprecated options:
+
+Game.HookSetCursorPos = false;
+Game.HookGetCursorPos = false;
+Game.HookGetKeyState = false;
+Game.HookGetAsyncKeyState = false;
+Game.HookGetKeyboardState = false;
+Game.HookFilterRawInput = false;
+Game.HookFilterMouseMessages = false;
+Game.HookUseLegacyInput = false;
+Game.HookDontUpdateLegacyInMouseMsg = false;
+Game.HookMouseVisibility = false;
+
+Game.SendNormalMouseInput = false;
+Game.SendNormalKeyboardInput = false;
+Game.SendScrollWheel = false;
+Game.ForwardRawKeyboardInput = false;
+Game.ForwardRawMouseInput = false;
+Game.HookReRegisterRawInput = false;
+Game.HookReRegisterRawInputMouse = false;
+Game.HookReRegisterRawInputKeyboard = false;
+Game.DrawFakeMouseCursor = false;
+
+//ProtoInput:
+
+Game.SupportsMultipleKeyboardsAndMice = true;
+
+Game.ProtoInput.InjectStartup = false;
+Game.ProtoInput.InjectRuntime_RemoteLoadMethod = false;
+Game.ProtoInput.InjectRuntime_EasyHookMethod = true;
+Game.ProtoInput.InjectRuntime_EasyHookStealthMethod = false;
+
+Game.LockInputAtStart = false;
+Game.LockInputSuspendsExplorer = true;
+Game.ProtoInput.FreezeExternalInputWhenInputNotLocked = true;
+Game.LockInputToggleKey = 0x23;
+
+Game.ProtoInput.RenameHandlesHook = false;
+Game.ProtoInput.RenameHandles = [];
+Game.ProtoInput.RenameNamedPipes = [];
+
+Game.ProtoInput.RegisterRawInputHook = false;
+Game.ProtoInput.GetRawInputDataHook = false;
+Game.ProtoInput.MessageFilterHook = false;
+Game.ProtoInput.GetCursorPosHook = false;
+Game.ProtoInput.SetCursorPosHook = false;
+Game.ProtoInput.GetKeyStateHook = false;
+Game.ProtoInput.GetAsyncKeyStateHook = false;
+Game.ProtoInput.GetKeyboardStateHook = false;
+Game.ProtoInput.CursorVisibilityHook = false;
+Game.ProtoInput.ClipCursorHook = true;
+Game.ProtoInput.FocusHooks = false;
+Game.ProtoInput.DrawFakeCursor = false;
+Game.ProtoInput.ClipCursorHookCreatesFakeClip = true;
+Game.ProtoInput.DontShowCursorWhenImageUpdated = true;
+
+Game.ProtoInput.RawInputFilter = false;
+Game.ProtoInput.MouseMoveFilter = false;
+Game.ProtoInput.MouseActivateFilter = false;
+Game.ProtoInput.WindowActivateFilter = false;
+Game.ProtoInput.WindowActvateAppFilter = false;
+Game.ProtoInput.MouseWheelFilter = false;
+Game.ProtoInput.MouseButtonFilter = false;
+Game.ProtoInput.KeyboardButtonFilter = false;
+
+Game.ProtoInput.SendMouseWheelMessages = true;
+Game.ProtoInput.SendMouseButtonMessages = true;
+Game.ProtoInput.SendMouseMovementMessages = true;
+Game.ProtoInput.SendKeyboardButtonMessages = true;
+Game.ProtoInput.XinputHook = true;
+Game.ProtoInput.UseOpenXinput = true;
+Game.ProtoInput.UseDinputRedirection = false;
+Game.ProtoInput.DinputDeviceHook = false;
+Game.ProtoInput.DinputHookAlsoHooksGetDeviceState = false;
+Game.ProtoInput.MultipleProtoControllers = false;
+
+Game.ProtoInput.EnableFocusMessageLoop = true;
+Game.ProtoInput.FocusLoopIntervalMilliseconds = 5000;
+Game.ProtoInput.FocusLoop_WM_ACTIVATE = true;
+Game.ProtoInput.FocusLoop_WM_ACTIVATEAPP = false;
+Game.ProtoInput.FocusLoop_WM_NCACTIVATE = false;
+Game.ProtoInput.FocusLoop_WM_SETFOCUS = false;
+Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
+Game.ProtoInput.BlockedMessages = [0x0008, 0x02a3, 0x02a1]; // Blocks WM_KILLFOCUS, WM_MOUSEHOVER and WM_MOUSELEAVE
+
+Game.ProtoInput.OnInputLocked = function() {
+  for (var i = 0; i < PlayerList.Count; i++) {
+    var player = PlayerList[i];
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetCursorPosHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.SetCursorPosHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetAsyncKeyStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyboardStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.CursorVisibilityStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetRawInputDataHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.RegisterRawInputHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.MessageFilterHookID);
+
+    //Avoid the mouse move filter unless absolutely necessary as it can massively affect performance if the game gets primary input from mouse move messages
+    //ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseMoveFilterID);
+
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.RawInputFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseActivateFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateAppFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
+
+    ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, true);
+  }
+};
+
+Game.ProtoInput.OnInputUnlocked = function() {
+  for (var i = 0; i < PlayerList.Count; i++) {
+    var player = PlayerList[i];
+
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetCursorPosHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.SetCursorPosHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetAsyncKeyStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyboardStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.CursorVisibilityStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetRawInputDataHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.RegisterRawInputHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.MessageFilterHookID);
+
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.RawInputFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseMoveFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseActivateFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateAppFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
+
+    ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, false);
+  }
+};
+
+Game.Play = function() {
+  var Args = (Context.Args = " -screen-fullscreen 0 -popupwindow " + " -screen-width " + Context.Width + " -screen-height " + Context.Height);
+
+  Context.StartArguments = Args;
+
+  var dllPath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\UnityPlayer.dll";
+
+  var searchPattern = "57 00 69 00 6E 00 64 00 6F 00 77 00 73 00 2E 00 47 00 61 00 6D 00 69 00 6E 00 67 00 2E 00 49 00 6E 00 70 00 75 00 74 00 2E 00 47 00 61 00 6D 00 65 00 70 00 61 00 64";
+  var patchPattern = "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00";
+
+  Context.PatchFileFindPattern(dllPath, dllPath, searchPattern, patchPattern, true);
+
+  var fillPath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Scrapnaut_Data\\globalgamemanagers";
+
+  var base = Context.Nickname;
+  var og_Path = "Scrapnaut";
+  // Create newPath by rearranging characters from base
+  var newPath = og_Path.substring(0, og_Path.length - 3) + base.charAt(0) + base.charAt(base.length - 1) + base.length;
+
+  // Ensure newPath is at least as long as og_Path
+  if (newPath.length < og_Path.length) {
+    while (newPath.length < og_Path.length) {
+      newPath += base.charAt(base.length - 1);
+    }
+  } else if (og_Path.length < newPath.length) {
+    // Ensure og_Path is at least 3 characters long
+    newPath = newPath.substring(0, og_Path.length);
+  }
+
+  Context.PatchFile(fillPath, fillPath, og_Path, newPath);
+
+  Context.EditRegKey("HKEY_CURRENT_USER", "SOFTWARE\\SpiffyBit\\"+newPath, "Screenmanager Fullscreen mode_h3630240806", 3, Nucleus.RegType.DWord);
+  Context.EditRegKey("HKEY_CURRENT_USER", "SOFTWARE\\SpiffyBit\\"+newPath, "Screenmanager Resolution Height_h2627697771", Context.Height, Nucleus.RegType.DWord);
+  Context.EditRegKey("HKEY_CURRENT_USER", "SOFTWARE\\SpiffyBit\\"+newPath, "Screenmanager Resolution Width_h182942802", Context.Width, Nucleus.RegType.DWord);
+  Context.EditRegKey("HKEY_CURRENT_USER", "SOFTWARE\\SpiffyBit\\"+newPath, "Screenmanager Resolution Use Native_h1405027254", 0, Nucleus.RegType.DWord);
+
+
+  var numPlayers = 0;
+
+  for (var i = 0; i < PlayerList.Count; i++) {
+    var player = PlayerList[i];
+
+    if (player.IsXInput && player.ScreenIndex !== -1) {
+      numPlayers++;
+    }
+    player.ProtoController1 = Context.GamepadId;
+    player.ProtoController2 = Context.GamepadId;
+    player.ProtoController3 = Context.GamepadId;
+    player.ProtoController4 = Context.GamepadId;
+    player.ProtoController5 = Context.GamepadId;
+    player.ProtoController6 = Context.GamepadId;
+    player.ProtoController7 = Context.GamepadId;
+    player.ProtoController8 = Context.GamepadId;
+    player.ProtoController9 = Context.GamepadId;
+  }
+};
