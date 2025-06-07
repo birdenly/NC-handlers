8268d19a2ab003dad2177cd181ddd930e0cf56ba
birdenly <jomacaldas@hotmail.com.br>
2025-06-07 14:24:33 -0300

    deTechTive 2112

---
+++ handlers/DeTechtive 2112.js
@@ -0,0 +1,222 @@
+var answers1 = ["30", "60", "90", "120", "144", "0"];
+Game.AddOption("Select the FPS cap.", "0 is for unlimited FPS", "fps", answers1);
+
+Game.FileSymlinkExclusions = ["steam_api64.dll", "steam_appid.txt"];
+Game.DirSymlinkExclusions = ["Engine\\Binaries\\ThirdParty\\Steamworks\\Steamv153\\Win64", "Detective\\Binaries\\Win64\\RedpointEOS", "Detective\\Binaries\\Win64"];
+
+Game.HandlerInterval = 500;
+Game.SymlinkExe = false;
+Game.SymlinkGame = true;
+Game.SymlinkFolders = true;
+Game.ExecutableName = "Detective-Win64-Shipping.exe";
+Game.SteamID = "2808800";
+Game.GUID = "DeTechtive 2112";
+Game.GameName = "DeTechtive 2112";
+Game.MaxPlayers = 4;
+Game.MaxPlayersOneMonitor = 4;
+Game.BinariesFolder = "Detective\\Binaries\\Win64";
+Game.Hook.ForceFocus = false;
+Game.Hook.ForceFocusWindowName = "DeTechtive 2112";
+Game.Hook.DInputEnabled = false;
+Game.Hook.XInputEnabled = false;
+Game.Hook.XInputReroute = false;
+Game.Hook.CustomDllEnabled = false;
+Game.XInputPlusDll = [];
+Game.Description =
+  "IMPOTANT: open your game once before trying split screen and reduce your graphics in the main game before using the handler\n\nHost a online game (NOT LAN) and join in with others\n\nNaming the lobby and joining the lobby require keyboard/mouse.\n\nAfter all the instances have launched, resized and positioned correctly, press the END key once to lock the input for all instances to have their own working cursor and keyboard. You need to left click each mouse to make the emulated cursors appear after locking the input. Press the END key again to unlock the input when you finish playing. You can also use CTRL+Q to close Nucleus and all its instances when the input is unlocked.";
+Game.PauseBetweenProcessGrab = 10;
+Game.PauseBetweenStarts = 15;
+
+Game.RefreshWindowAfterStart = true;
+Game.HookFocus = true;
+Game.HideTaskbar = true;
+
+Game.UseNucleusEnvironment = true;
+Game.UserProfileConfigPath = "AppData\\Local\\Detective\\Saved\\Config";
+Game.UserProfileSavePath = "AppData\\Local\\Detective\\Saved\\SaveGames";
+
+//USS deprecated options:
+
+Game.HookSetCursorPos = false;
+Game.HookGetCursorPos = false;
+Game.HookGetKeyState = false;
+Game.HookGetAsyncKeyState = false;
+Game.HookGetKeyboardState = false;
+Game.HookFilterRawInput = false;
+Game.HookFilterMouseMessages = false;
+Game.HookUseLegacyInput = false;
+Game.HookDontUpdateLegacyInMouseMsg = false;
+Game.HookMouseVisibility = false;
+
+Game.SendNormalMouseInput = false;
+Game.SendNormalKeyboardInput = false;
+Game.SendScrollWheel = false;
+Game.ForwardRawKeyboardInput = false;
+Game.ForwardRawMouseInput = false;
+Game.HookReRegisterRawInput = false;
+Game.HookReRegisterRawInputMouse = false;
+Game.HookReRegisterRawInputKeyboard = false;
+Game.DrawFakeMouseCursor = false;
+
+//ProtoInput:
+
+Game.SupportsMultipleKeyboardsAndMice = true;
+
+Game.ProtoInput.InjectStartup = true;
+Game.ProtoInput.InjectRuntime_RemoteLoadMethod = false;
+Game.ProtoInput.InjectRuntime_EasyHookMethod = true;
+Game.ProtoInput.InjectRuntime_EasyHookStealthMethod = false;
+
+Game.LockInputAtStart = false;
+Game.LockInputSuspendsExplorer = true;
+Game.ProtoInput.FreezeExternalInputWhenInputNotLocked = true;
+Game.LockInputToggleKey = 0x23;
+
+Game.ProtoInput.RenameHandlesHook = false;
+Game.ProtoInput.RenameHandles = [];
+Game.ProtoInput.RenameNamedPipes = [];
+
+Game.ProtoInput.RegisterRawInputHook = false;
+Game.ProtoInput.GetRawInputDataHook = false;
+Game.ProtoInput.MessageFilterHook = true;
+Game.ProtoInput.GetCursorPosHook = false;
+Game.ProtoInput.SetCursorPosHook = false;
+Game.ProtoInput.GetKeyStateHook = false;
+Game.ProtoInput.GetAsyncKeyStateHook = false;
+Game.ProtoInput.GetKeyboardStateHook = false;
+Game.ProtoInput.CursorVisibilityHook = false;
+// Game.ProtoInput.FocusHooks = true;
+Game.ProtoInput.ClipCursorHook = false;
+Game.ProtoInput.DrawFakeCursor = false;
+Game.ProtoInput.ClipCursorHookCreatesFakeClip = false;
+// Game.ProtoInput.SetWindowPosHook = true;
+// Game.ProtoInput.SetWindowStyleHook = true;
+
+Game.ProtoInput.RawInputFilter = false;
+Game.ProtoInput.MouseMoveFilter = false;
+Game.ProtoInput.MouseActivateFilter = false;
+Game.ProtoInput.WindowActivateFilter = false;
+Game.ProtoInput.WindowActvateAppFilter = false;
+Game.ProtoInput.MouseWheelFilter = false;
+Game.ProtoInput.MouseButtonFilter = false;
+Game.ProtoInput.KeyboardButtonFilter = false;
+
+Game.ProtoInput.SendMouseWheelMessages = true;
+Game.ProtoInput.SendMouseButtonMessages = true;
+Game.ProtoInput.SendMouseMovementMessages = true;
+Game.ProtoInput.SendKeyboardButtonMessages = true;
+Game.ProtoInput.XinputHook = true;
+Game.ProtoInput.UseOpenXinput = true;
+Game.ProtoInput.UseDinputRedirection = false;
+Game.ProtoInput.DinputDeviceHook = false;
+Game.ProtoInput.DinputHookAlsoHooksGetDeviceState = false;
+Game.ProtoInput.MultipleProtoControllers = false;
+
+Game.ProtoInput.EnableFocusMessageLoop = false;
+Game.ProtoInput.FocusLoopIntervalMilliseconds = 5000;
+Game.ProtoInput.FocusLoop_WM_ACTIVATE = true;
+// Game.ProtoInput.FocusLoop_WM_ACTIVATEAPP = false;
+// Game.ProtoInput.FocusLoop_WM_NCACTIVATE = false;
+// Game.ProtoInput.FocusLoop_WM_SETFOCUS = false;
+// Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
+Game.ProtoInput.BlockedMessages = [0x0008]; // Blocks WM_KILLFOCUS, WM_MOUSEHOVER and WM_MOUSELEAVE
+
+Game.ProtoInput.OnInputLocked = function() {
+  for (var i = 0; i < PlayerList.Count; i++) {
+    var player = PlayerList[i];
+
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetCursorPosHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.SetCursorPosHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetAsyncKeyStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyboardStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.CursorVisibilityStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetRawInputDataHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.RegisterRawInputHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.MessageFilterHookID);
+
+    //Avoid the mouse move filter unless absolutely necessary as it can massively affect performance if the game gets primary input from mouse move messages
+    //ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseMoveFilterID);
+
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.RawInputFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseActivateFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateAppFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
+
+    ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, true);
+
+    ProtoInput.StartFocusMessageLoop(player.ProtoInputInstanceHandle, 50, true, false, false, true, true);
+    System.Threading.Thread.Sleep(1000);
+    ProtoInput.StopFocusMessageLoop(player.ProtoInputInstanceHandle);
+  }
+};
+
+Game.ProtoInput.OnInputUnlocked = function() {
+  for (var i = 0; i < PlayerList.Count; i++) {
+    var player = PlayerList[i];
+
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetCursorPosHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.SetCursorPosHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetAsyncKeyStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyboardStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.CursorVisibilityStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetRawInputDataHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.RegisterRawInputHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.MessageFilterHookID);
+
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.RawInputFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseMoveFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseActivateFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateAppFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
+
+    ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, false);
+  }
+};
+
+Game.Play = function() {
+  var FPS = Context.Options["fps"];
+
+  Context.StartArguments = " -windowed -ResX= " + Context.Width + " -ResY= " + Context.Height;
+
+  Context.CopyScriptFolder(Context.GetFolder(Nucleus.Folder.InstancedGameFolder));
+
+  Context.Wait(1500);
+  var savePath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Engine\\Binaries\\ThirdParty\\Steamworks\\Steamv153\\Win64\\steam_settings\\configs.user.ini";
+  Context.ModifySaveFile(savePath, savePath, Nucleus.SaveType.INI, [
+  new Nucleus.IniSaveInfo("user::general", "account_name", Context.Nickname),
+  new Nucleus.IniSaveInfo("user::general", "account_steamid", Context.PlayerSteamID),
+  new Nucleus.IniSaveInfo("user::general", "language", Context.SteamLang),
+  ]);
+
+  var savePath = Context.EnvironmentPlayer + Context.UserProfileConfigPath + "\\Windows\\GameUserSettings.ini";
+  Context.ModifySaveFile(savePath, savePath, Nucleus.SaveType.INI, [
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "ResolutionSizeX", Context.Width),
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "ResolutionSizeY", Context.Height),
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "LastUserConfirmedResolutionSizeX", Context.Width),
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "LastUserConfirmedResolutionSizeY", Context.Height),
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "FullscreenMode", 2),
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "LastConfirmedFullscreenMode", 2),
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "PreferredFullscreenMode", 2),
+    new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "FrameRateLimit", FPS + ".000000")
+  ]);
+
+  if (Context.PlayerID == 0) {
+    var savePath = Context.EnvironmentPlayer + Context.UserProfileConfigPath + "\\Windows\\GameUserSettings.ini";
+    Context.ModifySaveFile(savePath, savePath, Nucleus.SaveType.INI, [
+      new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "MusicVolume", "1.000000"),
+    ]);
+  }else{
+    var savePath = Context.EnvironmentPlayer + Context.UserProfileConfigPath + "\\Windows\\GameUserSettings.ini";
+    Context.ModifySaveFile(savePath, savePath, Nucleus.SaveType.INI, [
+      new Nucleus.IniSaveInfo("/Script/Detective.CustomGameUserSettings", "MusicVolume", "0.000000"),
+    ]);
+  }
+};
