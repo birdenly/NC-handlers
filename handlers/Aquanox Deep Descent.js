059e8192fde4fdf9bb57d4a1c263291f225ebb8c
birdenly <jomacaldas@hotmail.com.br>
2025-10-14 23:03:53 -0300

    aquanox DD

---
+++ handlers/Aquanox Deep Descent.js
@@ -0,0 +1,225 @@
+var answers1 = ["30", "60", "90", "120", "144", "0"];
+Game.AddOption("Select the FPS cap.", "0 is for unlimited FPS", "fps", answers1);
+
+Game.FileSymlinkExclusions = ["steam_api64.dll", "steam_appid.txt"];
+Game.DirSymlinkExclusions = ["Engine\\Binaries\\ThirdParty\\Steamworks\\Steamv139\\Win64", "ANX\\Binaries\\Win64\\THQNOnline\\steam"];
+
+Game.HandlerInterval = 500;
+Game.SymlinkExe = false;
+Game.SymlinkGame = true;
+Game.SymlinkFolders = true;
+Game.ExecutableName = "ANX-Win64-Shipping.exe";
+Game.SteamID = "1501750";
+Game.GUID = "Aquanox Deep Descent";
+Game.GameName = "Aquanox Deep Descent";
+Game.MaxPlayers = 8;
+Game.MaxPlayersOneMonitor = 8;
+Game.BinariesFolder = "ANX\\Binaries\\Win64";
+Game.Hook.ForceFocus = false;
+Game.Hook.ForceFocusWindowName = "Aquanox: Deep Descent";
+Game.Hook.DInputEnabled = false;
+Game.Hook.XInputEnabled = true;
+Game.Hook.XInputReroute = false;
+Game.Hook.CustomDllEnabled = false;
+Game.XInputPlusDll = ["xinput1_3.dll"];
+Game.Description =
+  "IMPORTANT: Run you main game once and change some settings before trying splitscreen\n\nCreate a dogfight game in the online menu or start a campaign game and open it up to online. Dont make any of the lobbys private.\n\nAfter all the instances have launched, resized and positioned correctly, press the END key once to lock the input for all instances to have their own working cursor and keyboard. You need to left click each mouse to make the emulated cursors appear after locking the input. Press the END key again to unlock the input when you finish playing. You can also use CTRL+Q to close Nucleus and all its instances when the input is unlocked.";
+Game.PauseBetweenProcessGrab =5;
+Game.PauseBetweenStarts = 10;
+
+Game.HideTaskbar = true;
+Game.UseGoldberg = true;
+Game.SetWindowHook = true;
+Game.RefreshWindowAfterStart = true;
+// Game.SetForegroundWindowElsewhere = true;
+
+Game.UseNucleusEnvironment = true;
+Game.UserProfileConfigPath = "AppData\\Local\\ANX\\Saved\\Config";
+Game.UserProfileSavePath = "AppData\\Local\\ANX\\Saved\\SaveGames";
+
+//USS deprecated options:
+
+Game.HookSetCursorPos = false;
+Game.HookGetCursorPos = false;
+Game.HookGetKeyState = false;
+Game.HookGetAsyncKeyState = false;
+Game.HookGetKeyboardState = false;
+Game.HookFilterRawInput = false;
+Game.HookFilterMouseMessages = false;
+Game.HookUseLegacyInput = false;
+Game.HookDontUpdateLegacyInMouseMsg = false;
+Game.HookMouseVisibility = false;
+
+Game.SendNormalMouseInput = false;
+Game.SendNormalKeyboardInput = false;
+Game.SendScrollWheel = false;
+Game.ForwardRawKeyboardInput = false;
+Game.ForwardRawMouseInput = false;
+Game.HookReRegisterRawInput = false;
+Game.HookReRegisterRawInputMouse = false;
+Game.HookReRegisterRawInputKeyboard = false;
+Game.DrawFakeMouseCursor = false;
+
+//ProtoInput:
+
+Game.SupportsMultipleKeyboardsAndMice = true;
+
+Game.ProtoInput.InjectStartup = false;
+Game.ProtoInput.InjectRuntime_RemoteLoadMethod = false;
+Game.ProtoInput.InjectRuntime_EasyHookMethod = true;
+Game.ProtoInput.InjectRuntime_EasyHookStealthMethod = false;
+
+Game.LockInputAtStart = false;
+Game.LockInputSuspendsExplorer = true;
+Game.ProtoInput.FreezeExternalInputWhenInputNotLocked = true;
+Game.LockInputToggleKey = 0x23;
+
+Game.ProtoInput.RenameHandlesHook = false;
+Game.ProtoInput.RenameHandles = [];
+Game.ProtoInput.RenameNamedPipes = [];
+
+Game.ProtoInput.RegisterRawInputHook = false;
+Game.ProtoInput.GetRawInputDataHook = false;
+Game.ProtoInput.MessageFilterHook = true;
+Game.ProtoInput.GetCursorPosHook = false;
+Game.ProtoInput.SetCursorPosHook = false;
+Game.ProtoInput.GetKeyStateHook = false;
+Game.ProtoInput.GetAsyncKeyStateHook = false;
+Game.ProtoInput.GetKeyboardStateHook = false;
+Game.ProtoInput.CursorVisibilityHook = false;
+Game.ProtoInput.FocusHooks = true;
+Game.ProtoInput.ClipCursorHook = true;
+Game.ProtoInput.DrawFakeCursor = false;
+Game.ProtoInput.ClipCursorHookCreatesFakeClip = false;
+
+Game.ProtoInput.RawInputFilter = false;
+Game.ProtoInput.MouseMoveFilter = false;
+Game.ProtoInput.MouseActivateFilter = false;
+Game.ProtoInput.WindowActivateFilter = false;
+Game.ProtoInput.WindowActvateAppFilter = false;
+Game.ProtoInput.MouseWheelFilter = false;
+Game.ProtoInput.MouseButtonFilter = false;
+Game.ProtoInput.KeyboardButtonFilter = false;
+
+Game.ProtoInput.SendMouseWheelMessages = true;
+Game.ProtoInput.SendMouseButtonMessages = true;
+Game.ProtoInput.SendMouseMovementMessages = true;
+Game.ProtoInput.SendKeyboardButtonMessages = true;
+
+Game.ProtoInput.EnableFocusMessageLoop = true;
+Game.ProtoInput.FocusLoopIntervalMilliseconds = 8000;
+Game.ProtoInput.FocusLoop_WM_ACTIVATE = true;
+// Game.ProtoInput.FocusLoop_WM_ACTIVATEAPP = false;
+// Game.ProtoInput.FocusLoop_WM_NCACTIVATE = false;
+// Game.ProtoInput.FocusLoop_WM_SETFOCUS = false;
+// Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
+Game.ProtoInput.BlockedMessages = [0x0008]; // Blocks WM_KILLFOCUS, WM_MOUSEHOVER and WM_MOUSELEAVE
+
+Game.ProtoInput.OnInputLocked = function() {
+  for (var i = 0; i < PlayerList.Count; i++) {
+    var player = PlayerList[i];
+
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetCursorPosHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.SetCursorPosHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetAsyncKeyStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyboardStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.CursorVisibilityStateHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetRawInputDataHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.RegisterRawInputHookID);
+    ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.MessageFilterHookID);
+
+    //Avoid the mouse move filter unless absolutely necessary as it can massively affect performance if the game gets primary input from mouse move messages
+    //ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseMoveFilterID);
+
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.RawInputFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseActivateFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateAppFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
+    ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
+
+    ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, true);
+
+    //ProtoInput.StartFocusMessageLoop(player.ProtoInputInstanceHandle, 5000, true, true, true, true, true);
+  }
+};
+
+Game.ProtoInput.OnInputUnlocked = function() {
+  for (var i = 0; i < PlayerList.Count; i++) {
+    var player = PlayerList[i];
+
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetCursorPosHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.SetCursorPosHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetAsyncKeyStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetKeyboardStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.CursorVisibilityStateHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.GetRawInputDataHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.RegisterRawInputHookID);
+    ProtoInput.UninstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.MessageFilterHookID);
+
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.RawInputFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseMoveFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseActivateFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.WindowActivateAppFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
+    ProtoInput.DisableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
+
+    ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, false);
+  }
+};
+
+
+Game.Play = function() {
+  var FPS = Context.Options["fps"];
+
+  Context.StartArguments = " -windowed -nosplash -ResX= " + Context.Width + " -ResY= " + Context.Height;
+  
+
+  var savePath = Context.EnvironmentPlayer + Context.UserProfileConfigPath + "\\Windows\\GameUserSettings.ini";
+  Context.ModifySaveFile(savePath, savePath, Nucleus.SaveType.INI, [
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "ResolutionSizeX", Context.Width),
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "ResolutionSizeY", Context.Height),
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "LastUserConfirmedResolutionSizeX", Context.Width),
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "LastUserConfirmedResolutionSizeY", Context.Height),
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "FullscreenMode", 2),
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "LastConfirmedFullscreenMode", 2),
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "PreferredFullscreenMode", 2),
+    new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "FrameRateLimit", FPS + ".000000")
+  ]);
+
+  if (Context.PlayerID == 0) {
+    Context.ModifySaveFile(savePath, savePath, Nucleus.SaveType.INI, [
+      new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "CurrentMusicVolume", "100.000000"),
+    ])
+  }else{
+    Context.ModifySaveFile(savePath, savePath, Nucleus.SaveType.INI, [
+      new Nucleus.IniSaveInfo("/Script/ANX.ANX_GameUserSettings", "CurrentMusicVolume", "0.000000"),
+    ])
+  }
+
+    var ngincfg = Context.EnvironmentPlayer + Context.UserProfileConfigPath + "\\WindowsNoEditor\\Engine.ini";
+  Context.ModifySaveFile(ngincfg, ngincfg, Nucleus.SaveType.INI, [
+    new Nucleus.IniSaveInfo("/Script/Engine.LocalPlayer", "AspectRatioAxisConstraint", "AspectRatio_MaintainYFOV")
+  ]);
+
+  if (Context.AspectRatioDecimal < 1.2) {
+    Context.ModifySaveFile(ngincfg, ngincfg, Nucleus.SaveType.INI, [new Nucleus.IniSaveInfo("/Script/Engine.LocalPlayer", "AspectRatioAxisConstraint", "AspectRatio_MaintainXFOV")]);
+  }
+
+  if (!System.IO.Directory.Exists(Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\ANX\\Binaries\\Win64\\THQNOnline\\steam\\steam_settings")) {
+    var filePath = (Context.filePath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\ANX\\Binaries\\Win64\\THQNOnline\\steam\\steam_settings");
+    System.IO.Directory.CreateDirectory(filePath);
+  }
+
+  var autoExec = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\ANX\\Binaries\\Win64\\THQNOnline\\steam\\steam_settings\\DLC.txt";
+  var lines = [
+    "1422010=Aquanox Deep Descent - Ultimate Digital Kickstarter Edition",
+    "873780=Aquanox Deep Descent Digital Extras"
+  ];
+  Context.WriteTextFile(autoExec, lines);
+};
